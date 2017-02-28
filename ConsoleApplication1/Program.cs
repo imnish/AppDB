67bbf45454524179c2f18011865f6383e1f71701
nmish6 <nmishra9@sapient.com>
2017-02-28 13:15:53 +0530

    New Project Added

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MasterDAL dl = new MasterDAL();
+            dl.AddAgency("MediaVest");
+        }
+    }
+}
