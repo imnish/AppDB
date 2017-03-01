616a3f9879c180448bbe9aac0f824f7bc346524b
hmitt2 <hmittal3@sapient.com>
2017-03-01 12:24:30 +0530

    BaseDAL added

---
+++ EntityModel/AppDAL.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class AppDAL : BaseDAL
+    {
+        
+        #region
+        //App Data Operations
+        //public int AddApp(object tbl_App)
+        //{
+
+        //}
+
+        #endregion
+    }
+}
